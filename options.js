8d76e6540e2407179b387e3a720ecda794ae00a9
matze <matze@mdoetsch.de>
2015-03-03 21:29:51 +0100

    use iframe url from config

--- options.js
+++ options.js
@@ -1,18 +1,15 @@
-var el = document.getElementById('raspberry.host');
+var el     = document.getElementById('raspberry.host');
 var button = document.getElementById('host_save');
 
 chrome.storage.sync.get("host", function(config) {
-    console.log("store", config.host)
     el.value= config.host;
 });
 
 function save() {
-    console.log(el);
     var host = el.value;
-    alert(host);
 
     chrome.storage.sync.set({'host': host}, function() {
-        alert('Settings saved ' + host);
+        alert('Saved: ' + host);
     });
 }
 
--- popup.js
+++ popup.js
@@ -1,6 +1,9 @@
 
 function domLoaded() {
-
+    var iframe = document.getElementById('raspberry_iframe');
+    chrome.storage.sync.get("host", function(config) {
+        iframe.src = config.host;
+    });
 }
 
 document.addEventListener('DOMContentLoaded', domLoaded);
