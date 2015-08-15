e627d3403b867515381acb9e2a6ad4190a01c699
matze <matze@mdoetsch.de>
2015-08-15 13:44:56 +0200

    rename to "homie" + added bootstrap

--- options.js
+++ options.js
@@ -1,17 +1,13 @@
-var el     = document.getElementById('homie.host');
-var button = document.getElementById('host_save');
 
-chrome.storage.sync.get("host", function(config) {
-    el.value= config.host;
-});
-
-// todo use angularjs
-function save() {
-    var host = el.value;
-
-    chrome.storage.sync.set({'host': host}, function() {
-        alert('Saved: ' + host);
+angular.controller('OptionsController', function($scope) {
+    chrome.storage.sync.get("host", function(config) {
+        $scope.url = config.host;
+        $scope.$apply();
     });
-}
 
-button.onclick = save;
+    $scope.save = function() {
+        chrome.storage.sync.set({host: $scope.url}, function() {
+            alert('Saved: ' + host);
+        });
+    }
+});
